30fb24b1e1d78d6de94a343a9b06f80ca51a3369
patricklohn <patricklohn3@gmail.com>
2025-03-31 22:23:05 -0300

    componente de erros

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import Search from '../components/Search'
 import gitHub from '../axios/config'
 import { useState } from 'react'
 import { UserProps } from '../types/user'
 
+import Search from '../components/Search'
 import User from '../components/User'
+import Error from '../components/Error'
 
 const Home = () => {
   const [user,setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
+  const [messageError, setMessageError] = useState<string>("");
+
   const loadUser = async(userName: string) =>{
-    const res = await gitHub.get(`users/${userName}`)
-    //const res = await fetch(`https://api.github.com/users/${userName}`);
-    //const data = await res.json()
-    //console.log(data)
+    try {
+      const res = await gitHub.get(`users/${userName}`)
+
+      //const res = await fetch(`https://api.github.com/users/${userName}`);
+      //const data = await res.json()
+     //console.log(data)
+
     const {avatar_url, login, location, followers, following} = res.data;
     const userData: UserProps = {
       avatar_url,
@@ -23,11 +29,22 @@ const Home = () => {
     }
 
     setUser(userData);
+
+    } catch (error: unknown) {
+      if (typeof error === "object" && error !== null && "status" in error) {
+        if(error.status === 404){
+          setError(true)
+          setMessageError("Usuario não encontrado!")
+          return
+        }
+      }
+    }
   }
   return (
     <div>
       <Search loadUser={loadUser}/>
       {user && <User {...user}/>}
+      {error && <Error typeError={messageError}/>}
     </div>
   )
 }
