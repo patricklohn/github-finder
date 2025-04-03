359ba7f3add2edf9d68e771cbe55d0138ea8bc04
patricklohn <patricklohn3@gmail.com>
2025-04-03 07:56:59 -0300

    configurações iniciais para front end da tela repos

--- src/routes/Repos.tsx
+++ src/routes/Repos.tsx
@@ -5,31 +5,47 @@ import gitHub from '../axios/config'
 
 import classes from './Repos.module.css'
 import BackBtn from '../components/BackBtn';
+import Loader from '../components/Loader';
 
 const Repos =  () => {
     const {username} = useParams()
     const [repos, setRepos] = useState<RepoPros[] | [] | null>(null); 
-    const [isLoading, setisLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
       const loadRepos = async function(username: string){
-        setisLoading(true)
+        setIsLoading(true)
         const res = await  gitHub.get(`/users/${username}/repos`);
         //const res = await fetch(`https://api.github.com/users/${username}/repos`)
         //const data = await res.json() 
 
-        setisLoading(false)
-        console.log(res.data);
+        if(res){
+          setRepos(res.data);
+        }
+
+        setIsLoading(false)
       }
 
       if(username){
         loadRepos(username);
       }
+
     },[])
+
+    if(!repos && isLoading) return <Loader/>
+
   return (
     <div>
         <BackBtn/>
-        repos {username}
+        <h2>Explore os repositórios do usuário: {username}</h2>
+        {repos && repos.length === 0 && <p>Não há repositórios.</p>}
+        {repos && repos.length > 0 && (
+          <div>
+            {repos.map((repo: RepoPros) => (
+              <p>{repo.name}</p>
+            ))}
+          </div>
+        )}
     </div>
   )
 }
