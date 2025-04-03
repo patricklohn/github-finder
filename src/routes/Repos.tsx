9bc411ca728f3bbd015d373930ced951e9eba23d
patricklohn <patricklohn3@gmail.com>
2025-04-02 23:03:35 -0300

    criação de comunicação de api github

--- src/routes/Repos.tsx
+++ src/routes/Repos.tsx
@@ -1,12 +1,31 @@
 import {useState, useEffect} from 'react'; 
 import { useParams } from 'react-router-dom';
+import { RepoPros } from '../types/repo';
+import gitHub from '../axios/config'
 
 import classes from './Repos.module.css'
-import BackBtn from '../components/backBtn';
+import BackBtn from '../components/BackBtn';
 
-const Repos = () => {
+const Repos =  () => {
     const {username} = useParams()
-    
+    const [repos, setRepos] = useState<RepoPros[] | [] | null>(null); 
+    const [isLoading, setisLoading] = useState(false);
+
+    useEffect(() => {
+      const loadRepos = async function(username: string){
+        setisLoading(true)
+        const res = await  gitHub.get(`/users/${username}/repos`);
+        //const res = await fetch(`https://api.github.com/users/${username}/repos`)
+        //const data = await res.json() 
+
+        setisLoading(false)
+        console.log(res.data);
+      }
+
+      if(username){
+        loadRepos(username);
+      }
+    },[])
   return (
     <div>
         <BackBtn/>
